42f2eda4ff1144a427fa3cbfb29809070b993ca3
Alena Simoshina <alenasimoshina@yandex.ru>
2022-06-27 01:37:41 +0300

    пр13

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use((req, res, next) => {
   next();
 });
 
-mongoose.connect('mongodb://localhost:27017/mestodb');
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 
 app.use('/', require('./routes/cards'));
 app.use('/', require('./routes/users'));
