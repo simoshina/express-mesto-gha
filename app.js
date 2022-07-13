31325e36dbf6e04b7d438aa17c0497bed214ac9a
Alena Simoshina <alenasimoshina@yandex.ru>
2022-07-14 02:42:29 +0300

    пр14

--- app.js
+++ app.js
@@ -30,7 +30,8 @@ app.post('/signup', celebrate({
     password: Joi.string().required().min(8),
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
-    avatar: Joi.string(),
+    // eslint-disable-next-line no-useless-escape
+    avatar: Joi.string().pattern(/(http||https):\/\/([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:\/~+#-]*[\w@?^=%&\/~+#-])/),
   }),
 }), createUser);
 
--- controllers/users.js
+++ controllers/users.js
@@ -34,7 +34,12 @@ module.exports.createUser = (req, res, next) => {
   bcrypt.hash(password, 10).then((hash) => User.create({
     name, about, avatar, email, password: hash,
   }))
-    .then((user) => res.send(user))
+    .then((user) => res.send({
+      name: user.name,
+      about: user.about,
+      avatar: user.avatar,
+      email: user.email,
+    }))
     .catch((err) => {
       if (err.code === 11000) {
         return next(new ConflictError('Емейл занят'));
