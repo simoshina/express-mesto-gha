ddc7f571343b39d8ddcd5204924990403ef69674
Alena Simoshina <alenasimoshina@yandex.ru>
2022-07-14 20:22:29 +0300

    пр14

--- routes/cards.js
+++ routes/cards.js
@@ -7,9 +7,9 @@ const {
 router.get('/cards', getCards);
 router.post('/cards', celebrate({
   body: Joi.object().keys({
-    name: Joi.string().min(2).max(30),
+    name: Joi.string().min(2).max(30).required(),
     // eslint-disable-next-line no-useless-escape
-    link: Joi.string().pattern(/(http||https):\/\/([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:\/~+#-]*[\w@?^=%&\/~+#-])/),
+    link: Joi.string().pattern(/(http||https):\/\/([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:\/~+#-]*[\w@?^=%&\/~+#-])/).required(),
   }),
 }), createCard);
 router.delete('/cards/:cardId', celebrate({
